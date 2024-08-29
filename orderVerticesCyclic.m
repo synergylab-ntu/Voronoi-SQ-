3049d5b35a4fa3769702b0f122885835bd41b052
Itachi0791 <102822255+Itachi0791@users.noreply.github.com>
2024-08-29 20:20:40 +0530

    Delete orderVerticesCyclic.m

--- orderVerticesCyclic.m
+++
@@ -1,6 +0,0 @@
-function orderedVertices = orderVerticesCyclic(vertices)
-    % Compute convex hull
-    K = convhull(vertices(:,1), vertices(:,2));   
-    % Extract ordered vertices from convex hull
-    orderedVertices = vertices(K,:);
-end
