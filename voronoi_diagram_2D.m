c3ad0e0db46f2c78adb8b13e5a03c763efcd5e00
Itachi0791 <102822255+Itachi0791@users.noreply.github.com>
2024-08-29 19:59:53 +0530

    Update voronoi_diagram_2D.m
    changed to local optimization using initial condition, instead of global optimization

--- voronoi_diagram_2D.m
+++ voronoi_diagram_2D.m
@@ -126,20 +126,42 @@ function [xc1, yc1, xc2, yc2] = findClosestPoints(params1, params2)
     % Objective function for optimization
     objective = @(t) distanceBetweenSuperellipses(t(1), t(2), params1, params2);
 
-    % Bounds for t1 and t2
-    lb = [0, 0];
-    ub = [2*pi, 2*pi];
+    % % Bounds for t1 and t2
+    % lb = [0, 0];
+    % ub = [2*pi, 2*pi];
 
     % Use particleswarm for optimization
     %options = optimoptions('particleswarm', 'Display', 'off');
     % Optimization options for particleswarm
-    options = optimoptions('particleswarm', ...
-        'Display', 'off', ...           % Turn off display of iterations
-        'UseParallel', false, ...       % Do not use parallel computation
-        'SwarmSize', 100, ...           % Increase swarm size for more exploration
-        'MaxIterations', 1000, ...      % Increase maximum iterations
-        'FunctionTolerance', 1e-3);     % Tighten the tolerance for function change
-    [t_opt, ~] = particleswarm(objective, 2, lb, ub, options);
+    % options = optimoptions('particleswarm', ...
+    %     'Display', 'off', ...           % Turn off display of iterations
+    %     'UseParallel', false, ...       % Do not use parallel computation
+    %     'SwarmSize', 100, ...           % Increase swarm size for more exploration
+    %     'MaxIterations', 1000, ...      % Increase maximum iterations
+    %     'FunctionTolerance', 1e-3);     % Tighten the tolerance for function change
+    %[t_opt, ~] = particleswarm(objective, 2, lb, ub, options);
+
+     % Initial guess for alpha1 and alpha2 based on the line joining centers
+    h1_p = (params2.h - params1.h) * cos(params1.theta) + (params2.k - params1.k) * sin(params1.theta);
+    k1_p = -(params2.h - params1.h) * sin(params1.theta) + (params2.k - params1.k) * cos(params1.theta);
+    h2_p = (params1.h - params2.h) * cos(params2.theta) + (params1.k - params2.k) * sin(params2.theta);
+    k2_p = -(params1.h - params2.h) * sin(params2.theta) + (params1.k - params2.k) * cos(params2.theta);
+    tanalpha1 = abs(k1_p * params1.a / (h1_p * params1.b))^(1/params1.n) * sign(k1_p * params1.a / (h1_p * params1.b));
+    tanalpha2 = abs(k2_p * params2.a / (h2_p * params2.b))^(1/params2.n) * sign(k2_p * params2.a / (h2_p * params2.b));
+    alpha1 = atan(tanalpha1);
+    alpha2 = atan(tanalpha2);
+    % Adjust alpha1 and alpha2 if necessary
+    [x1, y1] = superellipse(alpha1, params1.a, params1.b, params1.n);
+    [x2, y2] = superellipse(alpha2, params2.a, params2.b, params2.n);
+    if sign(x1 * h1_p + y1 * k1_p) < 0
+        alpha1 = alpha1 + pi;
+    end
+    if sign(x2 * h2_p + y2 * k2_p) < 0
+        alpha2 = alpha2 + pi;
+    end
+
+    options = optimoptions('fminunc','Display', 'off');
+    t_opt = fminunc(objective, [alpha1, alpha2],options);
 
     % Calculate the closest points using the optimized parameters
     [x1, y1] = superellipse(t_opt(1), params1.a, params1.b, params1.n);
